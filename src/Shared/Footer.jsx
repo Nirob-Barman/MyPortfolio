ee0d4af45f82f402433b9e18069b9af6158f84d4
Nirob Barman <niroborin111@gmail.com>
2023-06-30 18:02:42 +0600

    Processing

--- src/Shared/Footer.jsx
+++ src/Shared/Footer.jsx
@@ -1,27 +1,13 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
 
 const Footer = () => {
     return (
-        <Box
-            component="footer"
-            sx={{
-                backgroundColor: '#f5f5f5',
-                padding: '20px',
-                textAlign: 'center',
-                position: 'fixed',
-                bottom: 0,
-                left: 0,
-                right: 0,
-            }}
-        >
-            <Typography variant="body2" color="textSecondary" component="p">
+        <footer className="bg-gray-100 py-4 text-center fixed bottom-0 left-0 right-0">
+            <p className="text-gray-500">
                 &copy; {new Date().getFullYear()} Nirob Barman
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-                MERN Stack Developer
-            </Typography>
-        </Box>
+            </p>
+            <p className="text-gray-500">MERN Stack Developer</p>
+        </footer>
     );
 };
 
